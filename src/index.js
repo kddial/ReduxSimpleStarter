479bdc83e88935703514397a510b86e2743920a3
Kevin Dial <kevin.dial.phone@gmail.com>
2016-09-28 23:35:16 -0400

    Added eslint to project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAVPPwmea3dCRpZ5zE1Y-5ss9vB1mhHvmc';
 
 // create a new component
 // this componenet should produce some html
 const App = () => {
-  return <div> Sickerrrrrrrrrrr </div>;
+  const render = (
+    <div>
+      Sickerrrrrrrrrrr
+      <SearchBar />
+    </div>
+  );
+
+  return render;
 }
 
 // REACT please take this
