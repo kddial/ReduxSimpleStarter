9344acb84ba475e20e58cc355fb6504f8e70db45
Kevin Dial <kevin.dial.phone@gmail.com>
2016-10-21 00:27:47 -0400

    Added production build

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,28 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: './resources/js',
+    filename: 'bundle.js',
+    publicPath: '/resources/js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url?limit=10000!img?progressive=true'
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
